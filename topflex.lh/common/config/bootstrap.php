d7030c8077192573b7846c488dc1bb1f76722490
Zub <zub.s@novaposhta.ua>
2017-05-18 16:51:33 +0300

    yii2-user

--- topflex.lh/common/config/bootstrap.php
+++ topflex.lh/common/config/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
+Yii::setAlias('@apps', dirname(dirname(__DIR__)). '/apps');
 Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+
+Yii::setAlias('@frontend', Yii::getAlias('@apps'). '/frontend');
+Yii::setAlias('@backend', Yii::getAlias('@apps'). '/backend');
+Yii::setAlias('@console', Yii::getAlias('@apps'). '/console');
+
+//Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
+//Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+//Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
